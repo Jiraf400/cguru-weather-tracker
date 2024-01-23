8cc1b859414a89bf068ce5dc17af657135dede14
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2024-01-23 22:19:53 +0300

    change current weather request method to POST

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { WeatherService } from './weather.service';
 import { Request, Response } from 'express';
 import { getWeatherDto } from './dto/get-weather.dto';
@@ -7,7 +7,7 @@ import { getWeatherDto } from './dto/get-weather.dto';
 export class WeatherController {
   constructor(private weatherService: WeatherService) {}
 
-  @Get('current_weather')
+  @Post('current_weather')
   async getCurrentWeather(@Req() req: Request, @Res() res: Response, @Body() dto: getWeatherDto) {
     if (!dto || !dto.apiToken || !dto.city) {
       return res.status(400).json({ error: 'All fields must be filled' });
